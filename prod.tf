c4b1831c74e8b3ccb2e99eb56a5b58e3a7ba582a
Ruben Flam-Shepherd <ruben.flam.shepherd@gmail.com>
2021-10-13 13:14:35 -0700

    added eip association and scaled up instances to a 2 count

--- prod.tf
+++ prod.tf
@@ -39,7 +39,9 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
-  ami = "ami-091dc5ac6b1d84e27"
+  count = 2
+
+  ami           = "ami-091dc5ac6b1d84e27"
   instance_type = "t2.nano"
 
   vpc_security_group_ids = [
@@ -52,10 +54,13 @@ resource "aws_instance" "prod_web" {
 
 }
 
-resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id
+resource "aws_eip_association" "prod_web" {
+  instance_id = aws_instance.prod_web.0.id
+  allocation_id = aws_eip.prod_web.id
+}
 
-  tags = {
+resource "aws_eip" "prod_web" {
+  tags = { 
     "Terraform" : "true"
   }  
 }
